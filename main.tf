b36c92b1974e98670606df1f1626113fce9eb90a
Okezie Nwaiwu <okezienwaiwu@yahoo.com>
2023-02-16 00:16:53 -0700

    renamed and updated main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA4ZVBDSSHJOCNHHL5"
-  secret_key = "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"
+  profile = "Okezie"
 }
 
 # Create an EC2 Instance
