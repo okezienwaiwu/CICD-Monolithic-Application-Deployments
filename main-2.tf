31b7fa444b160dd8bb3ea0688611a7f7ed458dd6
Okezie Nwaiwu <okezienwaiwu@yahoo.com>
2023-02-16 00:07:29 -0700

    Updated main.tf

--- main-2.tf
+++ main-2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region     = "us-east-2"
-  profile = "default"
-  
+  region     = "us-east-1"
+  access_key = "AKIA4ZVBDSSHJOCNHHL5"
+  secret_key = "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"
 }
 
 # Create an EC2 Instance
---
+++ my_main.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA4ZVBDSSHJOCNHHL5"
+  secret_key = "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"
+}
